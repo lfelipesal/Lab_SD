648ea80d0009edadfe9cb933a54a688c75027092
Felipe <93229568+lfelipesal@users.noreply.github.com>
2023-06-16 20:32:30 -0300

    Delete LSD11/LSD_guia_10_ERE_codigos/LSD12 directory

--- LSD11/LSD_guia_10_ERE_codigos/LSD12/Script.bat
+++
@@ -1,8 +0,0 @@
-cd C:\Users\luiz_\OneDrive\Área de Trabalho\LabSD\LSD11\
-ghdl -a divisor_clock.vhd
-ghdl -a semaforo.vhd
-ghdl -a tb_semaforo.vhd
-ghdl -e tb_semaforo
-ghdl -r tb_semaforo --vcd=tb_semaforo.vcd
-gtkwave -f tb_semaforo.vcd  --script=gtkwave_print.tcl
-:Done
