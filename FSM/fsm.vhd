c9f85d8492ab813a1118d662227f0996376490b8
Victor Hugo <vhduartelima@hotmail.com>
2023-07-13 20:17:43 -0300

    adding changes and image

--- FSM/fsm.vhd
+++ FSM/fsm.vhd
@@ -4,22 +4,22 @@ USE IEEE.numeric_std.ALL;
 
 ENTITY fsm IS
   PORT (
+    --entradas
     clock : IN STD_LOGIC;
     reset : IN STD_LOGIC;
     enter : IN STD_LOGIC;
     ficha : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
     produto : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-    -- produto_selecionado : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-    -- produto_valido : OUT STD_LOGIC;
-    valor_valido : OUT STD_LOGIC;
-    ficha_acumulada : OUT STD_LOGIC;
-    troco_calculado : OUT STD_LOGIC;
     tempo_expirado : IN STD_LOGIC;
-    next_state : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
-    display_troco : STD_LOGIC_VECTOR(7 DOWNTO 0);
-    display_codigo_produto : STD_LOGIC_VECTOR(7 DOWNTO 0);
-    display_valor_produto : STD_LOGIC_VECTOR(7 DOWNTO 0);
-    display_valor_acumulado : STD_LOGIC_VECTOR(7 DOWNTO 0);
+
+    --saidas
+    valor_valido : IN STD_LOGIC;
+    ficha_acumulada : IN STD_LOGIC;
+    troco_calculado : IN STD_LOGIC;
+    display_troco : OUT STD_LOGIC_VECTOR(0 TO 6);
+    display_codigo_produto : OUT STD_LOGIC_VECTOR(0 TO 6);
+    display_valor_produto : OUT STD_LOGIC_VECTOR(0 TO 6);
+    display_valor_acumulado : OUT STD_LOGIC_VECTOR(0 TO 6);
     led_end : OUT STD_LOGIC;
     led_insert : OUT STD_LOGIC;
   );
@@ -43,7 +43,6 @@ BEGIN
   PROCESS (clk, reset)
   BEGIN
     IF (RESET = '1') THEN
-
       current_state <= INITIAL;
     ELSIF rising_edge(clk) THEN
       current_state <= next_state;
