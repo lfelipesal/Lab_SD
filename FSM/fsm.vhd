7b7b1fd04e7b3482b0430585445df6b01c009a8f
Victor Hugo <vhduartelima@hotmail.com>
2023-07-13 22:37:33 -0300

    finalizando essa porra

--- FSM/fsm.vhd
+++ FSM/fsm.vhd
@@ -11,35 +11,115 @@ ENTITY fsm IS
     ficha : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
     produto : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
     tempo_expirado : IN STD_LOGIC;
-
-    --saidas
     valor_valido : IN STD_LOGIC;
     ficha_acumulada : IN STD_LOGIC;
     troco_calculado : IN STD_LOGIC;
-    display_troco : OUT STD_LOGIC_VECTOR(0 TO 6);
-    display_codigo_produto : OUT STD_LOGIC_VECTOR(0 TO 6);
-    display_valor_produto : OUT STD_LOGIC_VECTOR(0 TO 6);
-    display_valor_acumulado : OUT STD_LOGIC_VECTOR(0 TO 6);
+
+    --saidas
+    -- display_troco : OUT STD_LOGIC_VECTOR(0 TO 6);
+    -- display_codigo_produto : OUT STD_LOGIC_VECTOR(0 TO 6);
+    -- display_valor_produto : OUT STD_LOGIC_VECTOR(0 TO 6);
+    -- display_valor_acumulado : OUT STD_LOGIC_VECTOR(0 TO 6);
     led_end : OUT STD_LOGIC;
     led_insert : OUT STD_LOGIC;
   );
 END ENTITY fsm;
 
 ARCHITECTURE rtl OF fsm IS
+  COMPONENT data IS PORT (
+    --confirmar se todo signal precisa ser associado, a um entrada no componente fsm
+    --confirmar
+    ctrl : IN STD_LOGIC;
+    produto : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    ficha : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+    enter : IN STD_LOGIC;
+
+    clock : IN STD_LOGIC;
+    reset : IN STD_LOGIC;
+
+    ld_val_prod : OUT STD_LOGIC;
+    ld_troco : OUT STD_LOGIC;
+    ld_display_prod : OUT STD_LOGIC;
+    ld_display_val : OUT STD_LOGIC;
+    ld_display_val_ac : OUT STD_LOGIC;
+    ld_display_troco : OUT STD_LOGIC;
+    ld_acumulador : OUT STD_LOGIC;
+    ld_timer : OUT STD_LOGIC;
+    ficha_valida : OUT STD_LOGIC;
+    produto_valido : OUT STD_LOGIC;
+    compra_valida : OUT STD_LOGIC;
+    Cinco_time : OUT STD_LOGIC;
+
+    -- display_prod : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+    -- display_val : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+    -- display_val_ac : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+    -- display_troco : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)
+
+    );
+  END COMPONENT;
 
   TYPE state_type IS (inicial, produto_escolhido, valida_produto, produto_valido, recebe_ficha, valida_ficha, acumula_ficha, calcula_troco, espera_timer);
 
   SIGNAL current_state, next_state : state_type;
 
-  SIGNAL next_state_logic : STD_LOGIC;
+  SIGNAL clk_signal : STD_LOGIC;
+  SIGNAL reset_signal : STD_LOGIC;
 
-  SIGNAL ficha_valida_reg : STD_LOGIC;
+  SIGNAL ld_val_prod : STD_LOGIC;
+  SIGNAL ld_troco : STD_LOGIC;
+  SIGNAL ld_display_prod : STD_LOGIC;
+  SIGNAL ld_display_val : STD_LOGIC;
+  SIGNAL ld_display_val_ac : STD_LOGIC;
+  SIGNAL ld_display_troco : STD_LOGIC;
+  SIGNAL ld_acumulador : STD_LOGIC;
+  SIGNAL ld_timer : STD_LOGIC;
+  SIGNAL ficha_valida : STD_LOGIC;
+  SIGNAL produto_valido : STD_LOGIC;
+  SIGNAL compra_valida : STD_LOGIC;
+  SIGNAL Cinco_time : STD_LOGIC;
+  SIGNAL demux_signal : STD_LOGIC;
+  SIGNAL reg_prod : STD_LOGIC_VECTOR(3 DOWNTO 0);
+  SIGNAL reg_ficha : STD_LOGIC_VECTOR(3 DOWNTO 0);
+  SIGNAL reg_valor_prod : STD_LOGIC_VECTOR(3 DOWNTO 0);
+  SIGNAL reg_acu : STD_LOGIC_VECTOR(3 DOWNTO 0);
+  SIGNAL reg_troco : STD_LOGIC_VECTOR(3 DOWNTO 0);
 
-  SIGNAL ficha_acumulada_reg : STD_LOGIC;
-
-  SIGNAL calcula_troco_reg : STD_LOGIC;
-
+  SIGNAL display_prod : STD_LOGIC_VECTOR(6 DOWNTO 0);
+  SIGNAL display_val : STD_LOGIC_VECTOR(6 DOWNTO 0);
+  SIGNAL display_val_ac : STD_LOGIC_VECTOR(6 DOWNTO 0);
+  SIGNAL display_troco : STD_LOGIC_VECTOR(6 DOWNTO 0);
 BEGIN
+  my_data : data
+  PORT MAP(
+    ctrl => demux_signal;
+    produto => reg_prod;
+    ficha => reg_ficha;
+    enter => enter;
+
+    clock => clock;
+    reset => reset_signal;
+
+    ld_val_prod => ld_val_prod;
+    ld_troco => ld_troco;
+    ld_display_prod => ld_display_prod;
+    ld_display_val => ld_display_val;
+    ld_display_val_ac => ld_display_val_ac;
+    ld_display_troco => ld_display_troco;
+    ld_acumulador => ld_acumulador;
+    ld_timer => ld_timer;
+
+    ficha_valida => ficha_valida;
+    produto_valido => produto_valido;
+    compra_valida => compra_valida;
+
+    Cinco_time => Cinco_time;
+
+    display_prod => display_prod;
+    display_val => display_val;
+    display_val_ac => display_val_ac;
+    display_troco => display_troco;
+  )
+
   PROCESS (clk, reset)
   BEGIN
     IF (RESET = '1') THEN
@@ -47,5 +127,91 @@ BEGIN
     ELSIF rising_edge(clk) THEN
       current_state <= next_state;
     END IF;
+  END PROCESS;
+
+  PROCESS (current_state, clock, reset, enter, ficha, produto, tempo_expirado)
+  BEGIN
+    CASE current_state IS
+        --verificar se o current_state precisa ser trocado para next_state
+      WHEN inicial =>
+        ld_timer <= "0";
+        led_end <= "0";
+        IF (enter = "0") THEN
+          reset_signal <= "1";
+        ELSIF (enter = "1") THEN
+          reset_signal <= "0";
+          current_state <= produto_escolhido;
+        END IF;
+        -- ctrl <= "0";
+        -- produto <= "000";
+        -- ficha <= "0000";
+        -- enter <= "0";
+        -- clock <= "0";
+        -- ld_val_prod <= "0";
+        -- ld_troco <= "0";
+        -- ld_display_prod <= "0";
+        -- ld_display_val <= "0";
+        -- ld_display_val_ac <= "0";
+        -- ld_display_troco <= "0";
+        -- ld_acumulador <= "0";
+        -- ld_timer <= "0";
+        -- ficha_valida <= "0";
+        -- produto_valido <= "0";
+        -- compra_valida <= "0";
+        -- Cinco_time <= "0";
+      WHEN produto_escolhido =>
+        demux_signal <= '1';
+        reg_prod <= produto;
+        current_state <= valida_produto;
+
+      WHEN valida_produto =>
+        IF (produto_valido = "1") THEN
+          current_state <= produto_valido;
+        ELSE
+          current_state <= inicial;
+        END IF;
+
+      WHEN produto_valido =>
+        ld_display_prod <= "1";
+        current_state <= recebe_ficha;
+
+      WHEN recebe_ficha =>
+        IF (enter = "0") THEN
+          demux_signal <= "0";
+          reg_ficha <= ficha;
+          ld_display_val <= "1";
+          led_insert <= "1";
+        ELSE
+          current_state <= valida_ficha;
+        END IF;
+      WHEN valida_ficha =>
+        IF (ficha_valida = "1") THEN
+          current_state <= acumula_ficha;
+        ELSE
+          current_state <= recebe_ficha;
+        END IF;
+      WHEN acumula_ficha =>
+        ld_acumulador <= "1";
+        IF (compra_valida = "1") THEN
+          current_state <= calcula_troco;
+        ELSE
+          current_state <= recebe_ficha;
+        END IF;
+      WHEN calcula_troco =>
+        ld_troco <= "1";
+        ld_display_troco <= "1";
+        led_insert <= "0";
+        --verificar se o ld_timer e o led_end podem estar aqui ou precisam ir no espera_timer;
+        ld_timer <= "1";
+        led_end <= "1";
+        current_state <= espera_timer;
+      WHEN espera_timer =>
+        IF (Cinco_time = "1") THEN
+          current_state <= inicial;
+        ELSE
+          current_state <= espera_timer;
+        END IF;
+        --quando for o mesmo estado, eu preciso retornar ao mesmo estado no if, ou não é necessário?
+    END CASE;
   END PROCESS;
 END ARCHITECTURE rtl;
--- data.vhd
+++ data.vhd
@@ -1,108 +1,275 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
 
+ENTITY data IS
+    PORT (
+        ctrl : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        produto : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        ficha : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        enter : IN STD_LOGIC;
 
+        clock : IN STD_LOGIC;
+        reset : IN STD_LOGIC;
 
-entity data is
-    port(
-        ctrl : in std_logic_vector(2 downto 0);
-        produto : in std_logic_vector(2 downto 0);
-        ficha : in  std_logic_vector(3 downto 0);
-        enter : in std_logic;
+        ld_val_prod : IN STD_LOGIC;
+        ld_troco : IN STD_LOGIC;
+        ld_display_prod : IN STD_LOGIC;
+        ld_display_val : IN STD_LOGIC;
+        ld_display_val_ac : IN STD_LOGIC;
+        ld_display_troco : IN STD_LOGIC;
+        ld_acumulador : IN STD_LOGIC;
+        ld_timer : IN STD_LOGIC;
 
-        clock : in std_logic;
-        reset : in std_logic;
-    
-        ld_val_prod : in std_logic;
-        ld_troco : in std_logic;
-        ld_display_prod : in std_logic;
-        ld_display_val : in std_logic;
-        ld_display_val_ac : in std_logic;
-        ld_display_troco : in std_logic;
-        ld_acumulador : in std_logic;
-        ld_timer : in std_logic;
-        
+        ficha_valida : OUT STD_LOGIC;
+        produto_valido : OUT STD_LOGIC;
+        compra_valida : OUT STD_LOGIC;
 
-        ficha_valida : out std_logic;
-        produto_valido : out std_logic;
-        compra_valida : out std_logic;
+        Cinco_time : OUT STD_LOGIC;
 
+        display_prod : OUT STD_LOGIC_VECTOR(0 TO 6);
+        display_val : OUT STD_LOGIC_VECTOR(0 TO 6);
+        display_val_ac : OUT STD_LOGIC_VECTOR(0 TO 6);
+        display_troco : OUT STD_LOGIC_VECTOR(0 TO 6);
 
-        Cinco_time : out std_logic;
-
-        display_prod : out std_logic_vector(7 downto 0);
-        display_val : out std_logic_vector(7 downto 0);
-        display_val_ac : out std_logic_vector(7 downto 0);
-        display_troco : out std_logic_vector(7 downto 0)
-
-        );
-end;
-
-
-architecture rtl of data is
+    );
+END;
+ARCHITECTURE rtl OF data IS
     --declaração de componentes
-    component DivisorClock is
-        port 
-        (
-            CLOCK_50MHz : in std_logic;
-            reset	      : in std_logic;
-            CLOCK_1Hz   : out std_logic
+
+    COMPONENT ROM IS
+        PORT (
+            produto_escolhido : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+            preco_produto : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+            produto_existente : OUT STD_LOGIC
         );
-    
-    end component;
+    END COMPONENT;
 
-    component ROM is
-        port (
-            produto_escolhido: in std_logic_vector(3 downto 0);
-            preco_produto: out std_logic_vector(3 downto 0);
-            produto_existente: out std_logic
+    COMPONENT somador IS
+        PORT (
+            valor1 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+            valor2 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+            resultado : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
         );
-    end component;
+    END COMPONENT;
 
-    component somador is
-        port (
-            valor1: in std_logic_vector(3 downto 0);
-            valor2: in std_logic_vector(3 downto 0);
-            resultado: out std_logic_vector(3 downto 0)
+    COMPONENT subtrator IS
+        PORT (
+            valor1 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+            valor2 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+            resultado : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
         );
-    end component;
+    END COMPONENT;
 
-    component subtrator is
-        port (
-            valor1: in std_logic_vector(3 downto 0);
-            valor2: in std_logic_vector(3 downto 0);
-            resultado: out std_logic_vector(3 downto 0)
+    COMPONENT comparador_4 IS
+        PORT (
+            valor1 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+            valor2 : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+            lt : OUT STD_LOGIC;
+            gt : OUT STD_LOGIC;
+            eq : OUT STD_LOGIC
         );
-    end component;
+    END COMPONENT;
 
-    component comparador_4 is
-        port (
-            valor1: in std_logic_vector(3 downto 0);
-            valor2: in std_logic_vector(3 downto 0);
-            lt: out std_logic;
-            gt: out std_logic;
-            eq: out std_logic
+    COMPONENT contador IS
+        PORT (
+            clock : IN STD_LOGIC;
+            reset : IN STD_LOGIC;
+            gt_or_eq : OUT STD_LOGIC
         );
-    end component;
+    END COMPONENT;
 
-    component contador is
-        port (
-            clock: in std_logic;
-            reset: in std_logic;
-            gt_or_eq: out std_logic
+    COMPONENT conversor IS
+        PORT (
+            binary : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+            hex : OUT STD_LOGIC_VECTOR(0 TO 6)
         );
-    end component;
+    END COMPONENT;
 
-    component conversor is
-        port (
-            binary : in std_logic_vector(3 downto 0);
-            hex : out std_logic_vector(0 to 6)
+    COMPONENT valida_ficha IS
+        PORT (
+            reg_ficha : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+            ficha_valida : OUT STD_LOGIC
         );
-    end component;
+    END COMPONENT;
 
-    
-    
+    COMPONENT valida_prod IS
+        PORT (
+            reg_prod : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+            prod_valida : OUT STD_LOGIC
+        );
+    END COMPONENT;
 
-end architecture;
+    COMPONENT Demux IS
+        PORT (
+            enter : IN STD_LOGIC;
+            ctrl : IN STD_LOGIC;
+            ficha : OUT STD_LOGIC;
+            produto : OUT STD_LOGIC
+        );
+    END COMPONENT;
 
+    --declaração de sinais intermediários
+    SIGNAL reg_prod : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SIGNAL reg_ficha : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SIGNAL reg_valor_prod : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SIGNAL reg_acu : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SIGNAL reg_troco : STD_LOGIC_VECTOR(3 DOWNTO 0);
+
+    SIGNAL ld_ficha : STD_LOGIC;
+    SIGNAL ld_prod : STD_LOGIC;
+
+    SIGNAL rom_out : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SIGNAL somador_out : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SIGNAL subtrator_out : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SIGNAL display_acumulado_out : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SIGNAL display_produto_out : STD_LOGIC_VECTOR(0 TO 6);
+    SIGNAL display_preco_produto_out : STD_LOGIC_VECTOR(0 TO 6);
+    SIGNAL display_troco_out : STD_LOGIC_VECTOR(0 TO 6);
+
+    SIGNAL clk : STD_LOGIC;
+
+BEGIN
+    --instanciação de componentes
+
+    my_demux : Demux
+    PORT MAP(
+        enter => enter,
+        ctrl => ctrl,
+        ficha => ld_ficha,
+        produto => ld_prod
+    );
+
+    my_ROM : ROM
+    PORT MAP(
+        produto_escolhido => reg_prod,
+        preco_produto => rom_out,
+        produto_existente => produto_valido
+    );
+
+    my_somador : somador
+    PORT MAP(
+        valor1 => reg_ficha,
+        valor2 => reg_acu,
+        resultado => somador_out
+    );
+
+    my_subtrator : subtrator
+    PORT MAP(
+        valor1 => reg_acu,
+        valor2 => reg_valor_prod,
+        resultado => subtrator_out
+    );
+
+    my_comparador_4 : comparador_4
+    PORT MAP(
+        valor1 => reg_acu,
+        valor2 => reg_valor_prod,
+        lt => compra_valida
+    );
+
+    my_contador : contador
+    PORT MAP(
+        clock => clk,
+        reset => reset,
+        gt_or_eq => Cinco_time
+    );
+
+    my_conversor_produto : conversor
+    PORT MAP(
+        binary => reg_prod,
+        hex => display_produto_out
+    );
+
+    my_conversor_preco_produto : conversor
+    PORT MAP(
+        binary => reg_valor_prod,
+        hex => display_preco_produto_out
+    );
+
+    my_conversor_acumulado_e_troco : conversor
+    PORT MAP(
+        binary => display_acumulado_e_troco_in,
+        hex => display_acumulado_e_troco_out
+    );
+    my_register_ficha : registrador_4
+    PORT MAP(
+        load => ld_ficha,
+        reset => reset,
+        clock => clock,
+        entrada => ficha,
+        saida => reg_ficha
+    );
+    my_register_produto : registrador_3
+    PORT MAP(
+        load => ld_prod,
+        reset => reset,
+        clock => clock,
+        entrada => produto,
+        saida => reg_prod
+    );
+
+    my_register_valor_prod : registrador_3
+    PORT MAP(
+        load => ld_val_prod,
+        reset => reset,
+        clock => clock,
+        entrada => rom_out,
+        saida => reg_valor_prod
+    );
+
+    my_register_acumulador : registrador_4
+    PORT MAP(
+        load => ld_acumulador,
+        reset => reset,
+        clock => clock,
+        entrada => somador_out,
+        saida => reg_acu
+    );
+
+    my_register_troco : registrador_4
+    PORT MAP(
+        load => ld_troco,
+        reset => reset,
+        clock => clock,
+        entrada => subtrator_out,
+        saida => reg_troco
+    );
+
+    my_register_display_prod : registrador_7
+    PORT MAP(
+        load => ld_display_prod,
+        reset => reset,
+        clock => clock,
+        entrada => display_produto_out,
+        saida => display_prod
+    );
+
+    my_register_display_val : registrador_7
+    PORT MAP(
+        load => ld_display_val,
+        reset => reset,
+        clock => clock,
+        entrada => display_preco_produto_out,
+        saida => display_val
+    );
+
+    my_register_display_val_ac : registrador_7
+    PORT MAP(
+        load => ld_display_val_ac,
+        reset => reset,
+        clock => clock,
+        entrada => display_acumulado_out,
+        saida => display_val_ac
+    );
+
+    my_register_display_troco : registrador_7
+    PORT MAP(
+        load => ld_display_troco,
+        reset => reset,
+        clock => clock,
+        entrada => display_troco_out,
+        saida => display_troco
+    );
+
+END ARCHITECTURE;
